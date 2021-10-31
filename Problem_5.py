8d0a07636b447841d7216732aa392dc76cb02671
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-10-31 22:11:31 +0300

    last file

--- Problem_5.py
+++ Problem_5.py
@@ -6,13 +6,6 @@ def truth_value(X =[], Y =[]):
            else:
                print("False")
 
-    #     print(j%i)
-    # print(value)
-    # if value == len(X):
-    #     print("True")
-    # else:
-    #     print("False")
-
 X = [10,20,30,40]
 Y = [2,4,8,10]
 truth_value(X,Y)
