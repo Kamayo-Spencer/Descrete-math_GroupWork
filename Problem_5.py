6975e21c2524f760e3f8e23816bdb3788f006559
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-11-09 16:58:50 +0300

    New update

--- Problem_5.py
+++ Problem_5.py
@@ -1,12 +1,24 @@
-def truth_value(X =[], Y =[]):
+# Creating a function that accepts two lists as inputs
+def truth_value(X=[], Y=[]):
+    # A variable var that is initially 0 and would increment when a y that divides an X is found
+    val = 0
+    # looping through list X and Y and finding the modulus
     for i in Y:
         for j in X:
-           if j%i == 0:
-               print("True")
-           else:
-               print("False")
+            if j % i == 0:
+                val += 1
 
-X = [10,20,30,40]
-Y = [3,4,8,10]
-truth_value(X,Y)
+        # setting val to 0 if condition is met
+        if val != len(X):
+            val = 0
+        else:
+            print("True")
+            break
+    # Printing false if the condition is not met
+    if val != len(X):
+        print(False)
 
+
+X = [10, 20, 30, 40]
+Y = [3, 7, 9, 180]
+truth_value(X, Y)
